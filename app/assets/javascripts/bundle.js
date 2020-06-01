d17e9ec0d2cdd6c677f489cb7659055f5fe59b3e
zlin <liz.zxlin@gmail.com>
2020-06-01 16:39:44 -0700

    add favicon

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -3891,8 +3891,7 @@ __webpack_require__.r(__webpack_exports__);
 var getDate = function getDate() {
   var today = new Date();
   var dd = String(today.getDate()).padStart(2, '0');
-  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-
+  var mm = String(today.getMonth() + 1).padStart(2, '0');
   var yyyy = today.getFullYear();
   today = mm + '/' + dd + '/' + yyyy;
   return today;
